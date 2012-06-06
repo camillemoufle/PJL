052c4751d3ff1d67a8651039229828f4d6da8930
benchrih <benchrih.yassine@gmail.com>
2012-06-06 02:18:24 +0200

    hello

---
+++ TestGit2/src/test/GIT/TestGit2Activity.java
@@ -0,0 +1,13 @@
+package test.GIT;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestGit2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
